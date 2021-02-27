c5f4c52cf0dd8d59908d3f12cb8fc0d9a7e469ec
Cqoew <57765406+Cqoew@users.noreply.github.com>
2021-02-27 17:43:28 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogrenciler = new MyDictionary<int, string>();
+            ogrenciler.Add(1, "Ahnet");
+            ogrenciler.Add(2, "Ali");
+            ogrenciler.Add(3, "Bilal");
+            ogrenciler.Add(4, "Naim");
+            ogrenciler.Add(5, "Sevinç");
+
+            for (int i = 0; i < ogrenciler.Count(); i++)
+            {
+                Console.WriteLine(ogrenciler.keys[i] + " " + ogrenciler.values[i]);
+            }
+
+
+            Console.WriteLine("Öğrenci sayısı :" + ogrenciler.Count());
+
+
+
+        }
+    }
+}
